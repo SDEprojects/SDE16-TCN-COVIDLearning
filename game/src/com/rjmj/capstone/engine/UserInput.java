5724f1cd221ba588d04a14ee95df847cbfe63aad
neillperry <chainsaw.bucktooth@gmail.com>
2020-07-31 09:23:40 -0700

    added some changes to User Input class

--- game/src/com/rjmj/capstone/engine/UserInput.java
+++ game/src/com/rjmj/capstone/engine/UserInput.java
@@ -2,11 +2,16 @@ package com.rjmj.capstone.engine;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class UserInput {
 //    File file = new File("/Users/michaellennerblom/tlg-cap/capstone/game/src/com/rjmj/capstone/engine/input.txt");
-    Scanner sc = new Scanner(System.in);
+    private Scanner sc = new Scanner(System.in);
+    private final List<String> searchSynonyms = new ArrayList<String>(Arrays.asList(
+            "look", "examine", "inspect", "search"));
 
     public String getInput(String question){
         System.out.println(question);
