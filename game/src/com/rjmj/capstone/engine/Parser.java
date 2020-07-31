23439c0dc484d0c32ec3901390c3d8760264ba68
neillperry <chainsaw.bucktooth@gmail.com>
2020-07-31 14:53:50 -0700

    got the synonym parser to work. Kind of

--- game/src/com/rjmj/capstone/engine/Parser.java
+++ game/src/com/rjmj/capstone/engine/Parser.java
@@ -2,47 +2,63 @@ package com.rjmj.capstone.engine;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Parser {
-    private UserInput input = new UserInput();
+    private Scanner scanner;
 
     private ArrayList<String> searchSynonyms;
     private ArrayList<String> moveSynonyms;
     private ArrayList<String> useSynonyms;
+    private ArrayList<String> takeSynonyms;
+    private ArrayList<String> talkSynonyms;
 
     private String searchFileTitle;
     private String moveFileTitle;
     private String useFileTitle;
+    private String takeFileTitle;
+    private String talkFileTitle;
 
     private File searchFile;
     private File moveFile;
     private File useFile;
+    private File takeFile;
+    private File talkFile;
 
     private BufferedReader searchReader;
     private BufferedReader moveReader;
     private BufferedReader useReader;
+    private BufferedReader takeReader;
+    private BufferedReader talkReader;
 
     public Parser() throws IOException {
+        scanner = new Scanner(System.in);
+
         searchSynonyms = new ArrayList<>();
         moveSynonyms = new ArrayList<>();
         useSynonyms = new ArrayList<>();
+        takeSynonyms = new ArrayList<>();
+        talkSynonyms = new ArrayList<>();
 
         searchFileTitle = "searchSynonyms.txt";
         moveFileTitle = "moveSynonyms.txt";
         useFileTitle = "useSynonyms.txt";
+        takeFileTitle = "takeSynonyms.txt";
+        talkFileTitle = "talkSynonyms.txt";
 
-        searchFile = new File(getClass().getClassLoader().getResource(searchFileTitle).getFile());
-        moveFile = new File(getClass().getClassLoader().getResource(moveFileTitle).getFile());
-        useFile = new File(getClass().getClassLoader().getResource(useFileTitle).getFile());
-
-        searchReader = new BufferedReader(new FileReader(searchFile));
-        moveReader = new BufferedReader(new FileReader(moveFile));
-        useReader = new BufferedReader(new FileReader(useFile));
+        searchReader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/" + searchFileTitle)));
+        moveReader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/" + moveFileTitle)));
+        useReader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/" + useFileTitle)));
+        talkReader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/" + talkFileTitle)));
+        takeReader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/" + takeFileTitle)));
 
         instantiateSearchSynonymArray();
         instantiateMoveSynonymArray();
         instantiateUseSynonymArray();
+        instantiateTakeSynonymArray();
+        instantiateTalkSynonymArray();
     }
+
 
     private void instantiateSearchSynonymArray() throws IOException {
         try {
@@ -68,23 +84,41 @@ public class Parser {
         }
     }
 
-
-    //this pardner prompts the player for precepts (lol... it gets commands)
-    public String[] getParsedUserInput(String question) {
-        String command = input.getInput(question);
-        return command.toLowerCase().split("\\s+", 2);
+    private void instantiateTakeSynonymArray() throws IOException {
+        try {
+            while (takeReader.ready()) takeSynonyms.add(takeReader.readLine());
+        } finally {
+            System.out.println("Take array populated!");
+        }
     }
 
-    public ArrayList<String> getSearchSynonyms() {
-        return searchSynonyms;
+    private void instantiateTalkSynonymArray() throws IOException {
+        try {
+            while (talkReader.ready()) talkSynonyms.add(talkReader.readLine());
+        } finally {
+            System.out.println("Talk array populated!");
+        }
     }
 
-    public ArrayList<String> getMoveSynonyms() {
-        return moveSynonyms;
+
+    public Boolean isSearchSynonym(String word) {
+        return searchSynonyms.contains(word.toLowerCase());
     }
 
-    public ArrayList<String> getUseSynonyms() {
-        return useSynonyms;
+    public Boolean isMoveSynonym(String word) {
+        return moveSynonyms.contains(word.toLowerCase());
+    }
+
+    public Boolean isUseSynonym(String word) {
+        return useSynonyms.contains(word.toLowerCase());
+    }
+
+    public Boolean isTakeSynonym(String word) {
+        return takeSynonyms.contains(word.toLowerCase());
+    }
+
+    public Boolean isTalkSynonym(String word) {
+        return talkSynonyms.contains(word.toLowerCase());
     }
 
 }
