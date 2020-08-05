83c357fdb32cc24c47eb24afbf5533484cff8611
neillperry <chainsaw.bucktooth@gmail.com>
2020-08-05 12:46:37 -0700

    added Chandana code from yesterday to Dining Room

--- game/src/com/rjmj/capstone/room/StoryDiningRoom.java
+++ game/src/com/rjmj/capstone/room/StoryDiningRoom.java
@@ -8,9 +8,9 @@ public class StoryDiningRoom implements StoryRoom{
     private String nextAction;
 
     @Override
-    public void enter(Scanner scanner) {
+    public void enter(Scanner scanner) throws InterruptedException {
         this.scanner = scanner;
-        System.out.println("You are in DINING ROOM now");
+        diningIntro();
         System.out.println("What would you like to do");
         freeAns();
 
@@ -31,5 +31,25 @@ public class StoryDiningRoom implements StoryRoom{
     @Override
     public String getNextAction() {
         return nextAction;
+    }
+
+    public void diningIntro() throws InterruptedException {
+        String[] diningIntroText = {
+                "\u001B[35m",
+                "You are in the Dining room because you are dying of thirst.",
+                "There's no trace of water or food you see a glass on the table half filled with a liquid.",
+                "You find it fishy but you take a chance and drink.",
+                "Now you feel horrible with sudden fever, coughing and sweating, the fear of COVID virus is haunting you.",
+                "You are regretting the decision of drinking the liquid and badly looking around for some HELP!!",
+                "You are struggling to breath you feel half dead but you don't want to give up suddenly you hear some noises from the next room.",
+                "You are dying to seek help! You reach out to the door to open but your luck isn't favouring you today.The room is LOCKED!!!"
+                ,
+                "\u001B[0m"
+        };
+
+        for (String messages : diningIntroText) {
+            Thread.sleep(1000);
+            System.out.println(messages);
+        }
     }
 }
