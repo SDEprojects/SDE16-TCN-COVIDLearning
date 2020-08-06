96df6d7d7487c488b41e9800f24a3af9d1a4a735
takuhiro55 <takuhiro55@gmail.com>
2020-08-06 12:12:24 -0700

    StoryDiningRoom is now with ResourceBundle

--- game/src/com/rjmj/capstone/character/Color.java
+++ game/src/com/rjmj/capstone/character/Color.java
@@ -8,10 +8,12 @@ public interface Color {
     String ANSI_CYAN = "\u001B[36m";
     String ANSI_RED = "\u001B[31m";
     String ANSI_BLUE = "\u001B[34m";
+    String ANSI_PURPLE = "\u001B[35m";
+    String ANSI_GREEN = "\u001B[32m";
 
     /**
      * Coloring the fonts
-     * {0}:ANSI_CYAN, {1}:ANSI_RED, {2}:ANSI_BLUE, {3}:ANSI_RESET
+     * {0}:ANSI_CYAN, {1}:ANSI_RED, {2}:ANSI_BLUE, {3}:ANSI_RESET, {4}:ANSI_PURPLE
      */
     public default String textPainter(String text) {
         return MessageFormat.format(
@@ -19,7 +21,8 @@ public interface Color {
                 ANSI_CYAN,
                 ANSI_RED,
                 ANSI_BLUE,
-                ANSI_RESET);
+                ANSI_RESET,
+                ANSI_PURPLE);
     }
 
 }
