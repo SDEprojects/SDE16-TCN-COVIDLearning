aca8b286606b8dae3d48f640fc0fbaad084f408c
takuhiro55 <takuhiro55@gmail.com>
2020-08-06 13:22:04 -0700

    Applied Parser and ResourceBundle to TalkingTutorial

--- game/src/com/rjmj/capstone/character/Color.java
+++ game/src/com/rjmj/capstone/character/Color.java
@@ -10,10 +10,12 @@ public interface Color {
     String ANSI_BLUE = "\u001B[34m";
     String ANSI_PURPLE = "\u001B[35m";
     String ANSI_GREEN = "\u001B[32m";
+    String ANSI_YELLOW = "\u001B[33m";
+    String ANSI_WHITE = "\u001B[37m";
 
     /**
      * Coloring the fonts
-     * {0}:ANSI_CYAN, {1}:ANSI_RED, {2}:ANSI_BLUE, {3}:ANSI_RESET, {4}:ANSI_PURPLE
+     * {0}:ANSI_CYAN, {1}:ANSI_RED, {2}:ANSI_BLUE, {3}:ANSI_RESET, {4}:ANSI_PURPLE, {5}:ANSI_YELLOW
      */
     public default String textPainter(String text) {
         return MessageFormat.format(
@@ -22,7 +24,8 @@ public interface Color {
                 ANSI_RED,
                 ANSI_BLUE,
                 ANSI_RESET,
-                ANSI_PURPLE);
+                ANSI_PURPLE,
+                ANSI_YELLOW);
     }
 
 }
--- game/src/com/rjmj/capstone/tutorial/TalkingTutorial.java
+++ game/src/com/rjmj/capstone/tutorial/TalkingTutorial.java
@@ -1,74 +1,72 @@
 package com.rjmj.capstone.tutorial;
 
+import com.rjmj.capstone.character.Color;
+import com.rjmj.capstone.engines.MovementEngine;
+
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
-public class TalkingTutorial {
-    private Scanner userInput = new Scanner(System.in);
-    private final String ANSI_RESET = "\u001B[0m";
-    private final String ANSI_CYAN = "\u001B[36m";
-    private final String ANSI_RED = "\u001B[31m";
+public class TalkingTutorial implements InterfaceTutorial, Color {
 
-    public void startTalkingTutorial() throws InterruptedException {
+    //// For resource bundle ////
+    final String FILE_BASE_NAME = "TalkingTutorial";
+    ResourceBundle bundle = ResourceBundle.getBundle(PATH + FILE_BASE_NAME, Locale.US, rbc);
+    ////////////////////////////
+
+    private MovementEngine movementEngine = new MovementEngine();
+    private TutorialParser tutorialParser = new TutorialParser();
+    private Scanner userInput = new Scanner(System.in);
+
+    public void startTalkingTutorial(){
         tutorialIntroductionDisplay();
-        clearScreen();
+        movementEngine.clearScreen();
         talkingTutorialPrompt();
         talkingTutorialDisplay(talkingTutorialInputCollection());
     }
 
-    private void tutorialIntroductionDisplay() throws InterruptedException {
-        String[] tutorialIntroText = {
-                ANSI_CYAN,
-                "Hello there, welcome to the Apprenticeship Tutorial!",
-                "This tutorial will guide you through the basics of the game, how items work, and the general rules.",
-                "To get started, go ahead and type \"Start\" or any other command you prefer.",
-                ANSI_RESET
-        };
-
-        for (String introText : tutorialIntroText) {
-            Thread.sleep(700);
-            System.out.println(introText);
-        }
-
-        // This is just here to prompt the next action, no logic is done with the collected input.
-        userInput.next();
+    private void tutorialIntroductionDisplay() {
+        readStoryLinesOutOfFile("Welcome", 700);
+        userInput.nextLine();
     }
 
-    private void talkingTutorialPrompt() throws InterruptedException {
-        String[] talkingTutorialText = {
-                ANSI_CYAN,
-                "First up, lets learn how to talk to the various characters you will discover throughout the game.",
-                "This is pretty simple, just type \"Talk\" in the prompt to start a conversation with the available character.",
-                "Please note if there is no character in your current room, you will be unable to start a conversation.",
-                ANSI_RESET
-        };
-
-        for (String talkingText : talkingTutorialText) {
-            Thread.sleep(700);
-            System.out.println(talkingText);
-        }
+    private void talkingTutorialPrompt() {
+        readStoryLinesOutOfFile("First", 700);
     }
 
     private void talkingTutorialDisplay(String input) {
         if (input.equals("TALK")) {
-            System.out.println(ANSI_CYAN + "Jeannette: \"Hello there!  You did it.  Please type \"Next\" when you are ready, " +
-                    "or any other command you prefer to proceed to the next tutorial area.\"" + ANSI_RESET);
-
-            // This is just here to prompt the next action, lno logic is done with the collected input.
-            userInput.next();
-        } else {
-            System.out.println(ANSI_RED + "Sorry, invalid command entry, please try again using \"Talk\"" + ANSI_RESET);
+            System.out.println();
+            readStoryLinesOutOfFile("DidIt", 0);
+            userInput.next(); // This is used to take any key input
+        }
+        else {
+            readStoryLinesOutOfFile("Invalid", 0);
             talkingTutorialDisplay(talkingTutorialInputCollection());
         }
     }
 
-    private void clearScreen() throws InterruptedException {
-        Thread.sleep(1000);
-        for(int i = 0; i < 50; i++) {
-            System.out.println("\b");
-        }
+    private String talkingTutorialInputCollection() {
+        String[] result = new String[2];
+        String words = userInput.nextLine();
+        result = tutorialParser.parseAvailableActions(words);
+        return result[0];
     }
 
-    private String talkingTutorialInputCollection() {
-        return userInput.next().toUpperCase();
+    /** For accessing and displaying stories in Resource Bundle file */
+    public void readStoryLinesOutOfFile(String key, int SLEEP_DURATION_MS) {
+        String msg = null;
+        for (int i = 0; i < MAX_ITERATION_DISPLAY_STORIES; i++) {
+            try {
+                msg = textPainter(bundle.getString(key + "[" + i + "]"));
+                displayStoryLineByLine(msg, SLEEP_DURATION_MS);
+            } catch (MissingResourceException e) {
+                if (i == 0){
+                    System.out.println("Could not find the key : " + key);
+                }
+                break;
+            }
+        }
     }
 }
