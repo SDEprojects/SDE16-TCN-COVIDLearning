75d5b24690a342108e9f3151912acbc1ae4b54c1
takuhiro55 <takuhiro55@gmail.com>
2020-08-06 16:07:48 -0700

    Rebuilt com.rjmj.capstone

---
+++ game/test/com/rjmj/capstone/ExceptionMessageTest.java
@@ -0,0 +1,24 @@
+package com.rjmj.capstone;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExceptionMessageTest implements ExceptionMessage {
+
+//    private String testMessage;
+//
+//    @Test
+//    public void test(){
+//        try{
+//            getTestMessage();
+//        }
+//        catch(Exception e){
+//            somethingWentWrong(e);
+//        }
+//    }
+//
+//    public String getTestMessage() {
+//        return testMessage;
+//    }
+}
