19831f3ea0d83fdc6b58f32dc0068a984c4d02a0
takuhiro55 <takuhiro55@gmail.com>
2020-08-07 09:33:06 -0700

    Applied ResourceBundle to GameTextArt. Hierachy of Interface has been changed in room. Color->StoryRoomResourceBundle ->StoryRoom

--- game/src/com/rjmj/capstone/room/StoryHall.java
+++ game/src/com/rjmj/capstone/room/StoryHall.java
@@ -52,11 +52,15 @@ public class StoryHall implements StoryRoom {
             try {
                 msg = textPainter(bundle.getString(key + "[" + i + "]"));
                 displayStoryLineByLine(msg, SLEEP_DURATION_MS);
-            } catch (MissingResourceException e) {
+            }
+            catch (MissingResourceException e) {
                 if (i == 0){
                     System.out.println("Could not find the key : " + key);
                 }
                 break;
+            }
+            catch (Exception e){
+                somethingWentWrong(e);
             }
         }
     }
